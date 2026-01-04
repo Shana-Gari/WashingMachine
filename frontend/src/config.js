0e55b688b482d178ea48cde6203cf83737b744e6
ShanaGari <shanagari7@gmail.com>
2026-01-05 02:25:15 +0530

    Fix Render configuration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,10 @@
+let apiUrl = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+if (!apiUrl.startsWith('http')) {
+    apiUrl = `https://${apiUrl}`;
+}
+
 const config = {
-    API_URL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000"
+    API_URL: apiUrl
 };
 
 export default config;
